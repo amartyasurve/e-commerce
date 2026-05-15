87f06c8ff4b4573c430d994f566b58738c0b3b37
amartyasurve <amartyasurve@gmail.com>
2026-05-15 22:44:28 +0530

    initial steps followed

---
+++ eCommerce/src/main/java/com/project/ecommerce/EcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class EcommerceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApiApplication.class, args);
+    }
+}
